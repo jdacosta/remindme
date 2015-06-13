e015e4daf14e9cab82fed504e643e54a9e07fb78
Julien Martins Da Costa <contact@julien-dacosta.fr>
2015-06-14 00:03:16 +0200

    Add gulp data task

--- gulpfile.js/tasks/build-development.js
+++ gulpfile.js/tasks/build-development.js
@@ -4,7 +4,7 @@ var gulpSequence = require('gulp-sequence');
 gulp.task('build:development', function (callback) {
     gulpSequence(
         'clean',
-        ['icons', 'images', 'videos', 'sounds', 'fonts'],
+        ['icons', 'images', 'videos', 'sounds', 'fonts', 'data'],
         ['sass', 'browserify-desktop', 'browserify-mobile', 'html', 'server'],
         ['watch', 'browser-sync'],
         callback
--- gulpfile.js/tasks/build-production.js
+++ gulpfile.js/tasks/build-production.js
@@ -5,7 +5,7 @@ gulp.task('build:production', function (callback) {
     process.env.NODE_ENV = 'production';
     gulpSequence(
         'clean',
-        ['icons', 'images', 'videos', 'sounds', 'fonts'],
+        ['icons', 'images', 'videos', 'sounds', 'fonts', 'data'],
         ['sass', 'browserify-desktop', 'browserify-mobile', 'html', 'server'],
         callback
     );
